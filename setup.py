d2d7dd418fa77a14750d5265764b478402e86c94
andrewkrug <andrewkrug@gmail.com>
2017-07-18 17:43:02 -0700

    add plugins to distribution

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
+
 setup(
     name='mozilla-iam-cis',
     version='0.1dev',
-    packages=['cis'],
+    packages=['cis', 'cis/plugins'],
     license='MPL 2.0',
     long_description='Mozilla IAM change integration service',
     include_package_data=True
