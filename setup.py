7b29835d5c2b59ad30c123253347d7b063175b4c
andrewkrug <andrewkrug@gmail.com>
2017-08-02 16:15:08 -0700

    support coverage

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
-from distutils.core import setup
+from distutils.command.build import build
+from setuptools import setup
+from setuptools.command.install import install as _install
 
+import unittest
+
+def test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
+
+class install(_install):
+    def run(self):
+        self.run_command('build')
+        _install.run(self)
 
 setup(
     name='mozilla-iam-cis',
@@ -7,5 +20,6 @@ setup(
     packages=['cis', 'cis/plugins', 'cis/plugins/validation'],
     license='MPL 2.0',
     long_description='Mozilla IAM change integration service',
-    include_package_data=True
+    include_package_data=True,
+    test_suite=('setup.test_suite')
 )
