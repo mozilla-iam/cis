a5ddc9cf7f08807ab889d114ac47818f21dc1141
andrewkrug <andrewkrug@gmail.com>
2017-08-02 10:33:21 -0700

    refactor test to support encryption operation object

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -3,8 +3,6 @@ import json
 import os
 import unittest
 
-from unittest.mock import patch
-
 
 class EncryptionTest(unittest.TestCase):
 
@@ -12,6 +10,7 @@ class EncryptionTest(unittest.TestCase):
         fixtures = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/fixtures.json')
         with open(fixtures) as artifacts:
             self.test_artifacts = json.load(artifacts)
+
         os.environ['AWS_DEFAULT_REGION'] = self.test_artifacts['dummy_aws_region']
         # Set environment variables
         """
@@ -27,17 +26,36 @@ class EncryptionTest(unittest.TestCase):
         os.environ["CIS_KINESIS_STREAM_ARN"] = self.test_artifacts['dummy_kinesis_arn']
         os.environ["CIS_LAMBDA_VALIDATOR_ARN"] = self.test_artifacts['dummy_lambda_validator_arn']
 
-    @patch('cis.libs.encryption.kms')
-    @patch('cis.libs.encryption.os')
-    def test_encrypt_payload(self, mock_os, mock_kms):
+    def test_object_init(self):
+
+        from cis.libs import encryption
+
+        o = encryption.Operation(
+            boto_session = None
+        )
+
+        assert o is not None
+
+    def test_encryption(self):
+
+        from cis.libs import encryption
+
+        o = encryption.Operation(
+            boto_session = None
+        )
+
         test_kms_data = {
             'Plaintext': base64.b64decode(self.test_artifacts['Plaintext']),
             'CiphertextBlob': base64.b64decode(self.test_artifacts['CiphertextBlob'])
         }
+
         test_iv = base64.b64decode(self.test_artifacts['IV'])
 
-        mock_kms.generate_data_key.return_value = test_kms_data
-        mock_os.urandom.return_value = test_iv
+        # Set attrs on object to test data.  Not patching boto3 now!
+        o.data_key = test_kms_data
+        o.iv = test_iv
+
+        operation_result = o.encrypt(b'foobar')
 
         expected_result = {
             'ciphertext': base64.b64decode(self.test_artifacts['expected_ciphertext']),
@@ -46,23 +64,27 @@ class EncryptionTest(unittest.TestCase):
             'tag': base64.b64decode(self.test_artifacts['expected_tag'])
         }
 
-        from cis.libs.encryption import encrypt_payload
-        self.assertEqual(expected_result, encrypt_payload(b'foobar'))
-        mock_kms.generate_data_key.assert_called_once_with(
-            KeyId=self.test_artifacts['dummy_kms_arn'],
-            KeySpec='AES_256',
-            EncryptionContext={}
+        self.assertEqual(operation_result, expected_result)
+
+    def test_decryption(self):
+
+        from cis.libs import encryption
+
+        o = encryption.Operation(
+            boto_session = None
         )
 
-    @patch('cis.libs.encryption.kms')
-    def test_decrypt_payload(self, mock_kms):
         test_kms_data = {
             'Plaintext': base64.b64decode(self.test_artifacts['Plaintext']),
             'CiphertextBlob': base64.b64decode(self.test_artifacts['CiphertextBlob'])
         }
-        mock_kms.decrypt.return_value = test_kms_data
 
         test_iv = base64.b64decode(self.test_artifacts['IV'])
+
+        # Set attrs on object to test data.  Not patching boto3 now!
+        o.data_key = test_kms_data
+        o.iv = test_iv
+        o.plaintext_key = test_kms_data['Plaintext']
 
         kwargs = {
             'ciphertext': base64.b64decode(self.test_artifacts['expected_ciphertext']),
@@ -71,9 +93,13 @@ class EncryptionTest(unittest.TestCase):
             'tag': base64.b64decode(self.test_artifacts['expected_tag'])
         }
 
-        from cis.libs.encryption import decrypt_payload
-        self.assertEqual(decrypt_payload(**kwargs), b'foobar')
-        mock_kms.decrypt.assert_called_once_with(
-            CiphertextBlob=test_kms_data['CiphertextBlob'],
-            EncryptionContext={}
-        )
+        operation_result = o.decrypt(**kwargs)
+
+        expected_result = {
+            'ciphertext': base64.b64decode(self.test_artifacts['expected_ciphertext']),
+            'ciphertext_key': test_kms_data['CiphertextBlob'],
+            'iv': test_iv,
+            'tag': base64.b64decode(self.test_artifacts['expected_tag'])
+        }
+
+        self.assertEqual(operation_result, b'foobar')
