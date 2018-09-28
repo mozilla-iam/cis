f263657af7e94828f400b22b041988740ace0110
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-09-27 17:40:35 -0700

    fix relative imports

--- python-modules/cis_aws/cis_aws/__init__.py
+++ python-modules/cis_aws/cis_aws/__init__.py
@@ -7,8 +7,5 @@ import os
 
 from cis_aws import common
 from cis_aws import connect
-from everett.manager import ConfigManager
-from everett.manager import ConfigIniEnv
-from everett.manager import ConfigOSEnv
 
 __all__ = [connect, common]
