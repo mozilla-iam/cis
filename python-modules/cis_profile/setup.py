59238f4ffeaedc295e50efbb64d527fa18180e51
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-11-07 11:57:39 -0800

    support uploading to pypi & fix license

--- python-modules/cis_profile/setup.py
+++ python-modules/cis_profile/setup.py
@@ -26,7 +26,7 @@ setup(
     url="https://github.com/mozilla-iam/cis",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Mozilla Public License",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
     install_requires=requirements,
