ff66d1b7a3debbf3a0a96533ec5407b8e6a9984c
andrewkrug <akrug@mozilla.com>
2018-11-07 13:14:10 -0800

    bump version

--- python-modules/cis_profile/setup.py
+++ python-modules/cis_profile/setup.py
@@ -18,7 +18,7 @@ extras = {'test': test_requirements}
 
 setup(
     name="cis_profile",
-    version="0.0.1",
+    version="0.0.2",
     author="Guillaume Destuynder",
     author_email="kang@mozilla.com",
     description="Mozilla IAM user profile ('v2') class utility.",
