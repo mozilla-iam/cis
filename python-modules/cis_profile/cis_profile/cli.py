853a0c05aff9d150e3b0bbd41bf3913addf857a4
Florian Merz <flomerz@gmail.com>
2019-01-17 10:21:05 -0800

    Add cli config options to faker.

--- python-modules/cis_profile/cis_profile/cli.py
+++ python-modules/cis_profile/cis_profile/cli.py
@@ -2,7 +2,17 @@ import click
 import json
 import sys
 
-from cis_profile.fake_profile import FakeUser, batch_create_fake_profiles
+from cis_profile.fake_profile import (
+    FakeUser,
+    batch_create_fake_profiles,
+    FakeProfileConfig,
+)
+
+CONFIG_OPTIONS = [
+    f
+    for f in dir(FakeProfileConfig)
+    if callable(getattr(FakeProfileConfig, f)) and not f.startswith("__")
+]
 
 
 @click.group()
@@ -12,17 +22,31 @@ def main():
 
 
 @click.command()
-@click.option("--seed", type=int, help="seed to create random profile", default=None)
-def create(seed):
+@click.option(
+    "--seed", "-s", type=int, help="seed to create random profile", default=None
+)
+@click.option(
+    "--config",
+    "-c",
+    type=str,
+    multiple=True,
+    help="config options [{}]".format(", ".join(CONFIG_OPTIONS), default="default"),
+)
+def create(seed, config):
     """Create single IAM profile v2 object."""
 
-    u = FakeUser(seed=seed)
+    faker_config = FakeProfileConfig()
+    for c in config:
+        getattr(faker_config, c)()
+    u = FakeUser(seed=seed, config=faker_config)
     click.echo(json.dumps(u.as_dict(), indent=2))
 
 
 @click.command()
-@click.option("--seed", type=int, help="seed to create random profile", default=1337)
-@click.option("--count", type=int, help="how many profiles to create")
+@click.option(
+    "--seed", "-s", type=int, help="seed to create random profile", default=1337
+)
+@click.option("--number", "-n", type=int, help="how many profiles to create")
 def batch(seed, count):
     """Create many IAM profile v2 objects."""
     profiles = batch_create_fake_profiles(seed, count)
