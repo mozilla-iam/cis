f2c7eed25e78cf14f54c91d5b03da2c6b9487842
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-01-18 21:05:10 -0800

    black + fix missing uuid in change service

--- e2e/test_change_endpoint.py
+++ e2e/test_change_endpoint.py
@@ -7,26 +7,23 @@ from cis_profile import fake_profile
 from cis_profile import WellKnown
 
 
-cis_environment = os.getenv('CIS_ENVIRONMENT', 'testing')
-client_id_name = '/iam/cis/{}/change_client_id'.format(cis_environment)
-client_secret_name = '/iam/cis/{}/change_service_client_secret'.format(cis_environment)
+cis_environment = os.getenv("CIS_ENVIRONMENT", "testing")
+client_id_name = "/iam/cis/{}/change_client_id".format(cis_environment)
+client_secret_name = "/iam/cis/{}/change_service_client_secret".format(cis_environment)
 
-if cis_environment == 'testing':
-    base_url = 'change.api.test.sso.allizom.org'
-elif cis_environment == 'development':
-    base_url = 'change.api.dev.sso.allizom.org'
-elif cis_environment == 'production':
-    base_url == 'change.api.sso.mozilla.com'
+if cis_environment == "testing":
+    base_url = "change.api.test.sso.allizom.org"
+elif cis_environment == "development":
+    base_url = "change.api.dev.sso.allizom.org"
+elif cis_environment == "production":
+    base_url == "change.api.sso.mozilla.com"
 
-client = boto3.client('ssm')
+client = boto3.client("ssm")
 
 
 def get_secure_parameter(parameter_name):
-    response = client.get_parameter(
-        Name=parameter_name,
-        WithDecryption=True
-    )
-    return response['Parameter']['Value']
+    response = client.get_parameter(Name=parameter_name, WithDecryption=True)
+    return response["Parameter"]["Value"]
 
 
 def get_client_secret():
@@ -43,25 +40,25 @@ def exchange_for_access_token():
         client_id=get_client_id(),
         client_secret=get_client_secret(),
         audience="api.test.sso.allizom.org",
-        grant_type="client_credentials"
+        grant_type="client_credentials",
     )
 
     payload = json.dumps(payload_dict)
-    headers = {'content-type': "application/json"}
+    headers = {"content-type": "application/json"}
     conn.request("POST", "/oauth/token", payload, headers)
     res = conn.getresponse()
     data = json.loads(res.read())
-    return data['access_token']
+    return data["access_token"]
 
 
 def test_api_is_alive():
     access_token = exchange_for_access_token()
     conn = http.client.HTTPSConnection(base_url)
-    headers = {'authorization': "Bearer {}".format(access_token)}
+    headers = {"authorization": "Bearer {}".format(access_token)}
     conn.request("GET", "/v2/user/status?sequenceNumber=123456", headers=headers)
     res = conn.getresponse()
     data = json.loads(res.read())
-    assert data['identity_vault'] is not None
+    assert data["identity_vault"] is not None
 
 
 def test_publishing_a_profile_it_should_be_accepted():
@@ -71,10 +68,7 @@ def test_publishing_a_profile_it_should_be_accepted():
     jsonschema.validate(json.loads(json_payload), wk.get_schema())
     access_token = exchange_for_access_token()
     conn = http.client.HTTPSConnection(base_url)
-    headers = {
-        'authorization': "Bearer {}".format(access_token),
-        'Content-type': 'application/json'
-    }
+    headers = {"authorization": "Bearer {}".format(access_token), "Content-type": "application/json"}
     conn.request("POST", "/v2/user", json_payload, headers=headers)
     res = conn.getresponse()
     data = res.read()
@@ -89,10 +83,7 @@ def test_publishing_profiles_it_should_be_accepted():
     wk = WellKnown()
     access_token = exchange_for_access_token()
     conn = http.client.HTTPSConnection(base_url)
-    headers = {
-        'authorization': "Bearer {}".format(access_token),
-        'Content-type': 'application/json'
-    }
+    headers = {"authorization": "Bearer {}".format(access_token), "Content-type": "application/json"}
     conn.request("POST", "/v2/users", json.dumps(profiles), headers=headers)
     res = conn.getresponse()
     data = res.read()
--- python-modules/cis_change_service/cis_change_service/profile.py
+++ python-modules/cis_change_service/cis_change_service/profile.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 class Vault(object):
     """Handles flushing profiles to Dynamo when running local or in stream bypass mode."""
+
     def __init__(self, sequence_number=None):
         self.connection_object = connect.AWS()
         self.identity_vault_client = None
@@ -33,10 +34,10 @@ class Vault(object):
     def _verify(self, profile_json):
         cis_profile = User(user_structure_json=profile_json)
         try:
-            if self.config('verify_publishers', namespace='cis') == 'true':
+            if self.config("verify_publishers", namespace="cis") == "true":
                 cis_profile.verify_all_publishers()
 
-            if self.config('verify_signatures', namespace='cis') == 'true':
+            if self.config("verify_signatures", namespace="cis") == "true":
                 cis_profile.verify_all_signatures()
         except SignatureVerificationFailure:
             return False
@@ -54,24 +55,23 @@ class Vault(object):
         verified = self._verify(profile_json)
 
         if verified:
-            if self.config('dynamodb_transactions', namespace='cis') == 'true':
+            if self.config("dynamodb_transactions", namespace="cis") == "true":
                 vault = user.Profile(
-                    self.identity_vault_client.get('table'),
-                    self.identity_vault_client.get('client'),
-                    transactions=True
+                    self.identity_vault_client.get("table"), self.identity_vault_client.get("client"), transactions=True
                 )
             else:
                 vault = user.Profile(
-                    self.identity_vault_client.get('table'),
-                    self.identity_vault_client.get('client'),
-                    transactions=False
+                    self.identity_vault_client.get("table"),
+                    self.identity_vault_client.get("client"),
+                    transactions=False,
                 )
 
             user_profile = dict(
-                id=profile_json['user_id']['value'],
-                primary_email=profile_json['primary_email']['value'],
+                id=profile_json["user_id"]["value"],
+                primary_email=profile_json["primary_email"]["value"],
+                uuid=profile_json["uuid"]["value"],
                 sequence_number=self.sequence_number,
-                profile=json.dumps(profile_json)
+                profile=json.dumps(profile_json),
             )
 
             res = vault.find_or_create(user_profile)
@@ -84,19 +84,15 @@ class Vault(object):
         """Write profile to the identity vault."""
         self._connect()
 
-        if self.config('dynamodb_transactions', namespace='cis') == 'true':
-            logger.info('Attempting to put batch of profiles using transacations.')
+        if self.config("dynamodb_transactions", namespace="cis") == "true":
+            logger.info("Attempting to put batch of profiles using transacations.")
             vault = user.Profile(
-                self.identity_vault_client.get('table'),
-                self.identity_vault_client.get('client'),
-                transactions=True
+                self.identity_vault_client.get("table"), self.identity_vault_client.get("client"), transactions=True
             )
         else:
-            logger.info('Attempting to put batch of profiles without transactions.')
+            logger.info("Attempting to put batch of profiles without transactions.")
             vault = user.Profile(
-                self.identity_vault_client.get('table'),
-                self.identity_vault_client.get('client'),
-                transactions=False
+                self.identity_vault_client.get("table"), self.identity_vault_client.get("client"), transactions=False
             )
 
         user_profiles = []
@@ -107,33 +103,35 @@ class Vault(object):
 
             verified = self._verify(profile_json)
             if verified:
-                logger.info('Profiles have been verified. Constructing dictionary for storage.')
+                logger.info("Profiles have been verified. Constructing dictionary for storage.")
                 user_profile = dict(
-                    id=profile_json['user_id']['value'],
-                    primary_email=profile_json['primary_email']['value'],
+                    id=profile_json["user_id"]["value"],
+                    primary_email=profile_json["primary_email"]["value"],
+                    uuid=profile_json["uuid"]["value"],
                     sequence_number=self.sequence_number,
-                    profile=json.dumps(profile_json)
+                    profile=json.dumps(profile_json),
                 )
                 user_profiles.append(user_profile)
             else:
                 # XXX TBD Do something else
                 pass
-        logger.info('Attempting to send batch of {} profiles as a transaction.'.format(len(user_profiles)))
+        logger.info("Attempting to send batch of {} profiles as a transaction.".format(len(user_profiles)))
         return vault.find_or_create_batch(user_profiles)
 
     def _get_id(self, profile_json):
         if isinstance(profile_json, str):
             profile_json = json.loads(profile_json)
-        return profile_json.get('user_id').get('value').lower()
+        return profile_json.get("user_id").get("value").lower()
 
     def _get_primary_email(self, profile_json):
         if isinstance(profile_json, str):
             profile_json = json.loads(profile_json)
-        return profile_json.get('primary_email').get('value').lower()
+        return profile_json.get("primary_email").get("value").lower()
 
 
 class Status(object):
     """Does the right thing to query if the event was integrated and return the results."""
+
     def __init__(self, sequence_number):
         self.connection_object = connect.AWS()
         self.identity_vault_client = None
@@ -149,16 +147,14 @@ class Status(object):
         # Vault returns a dictionary of dictionaries for the statuses of each check.
         self._connect()
 
-        client = self.identity_vault_client.get('client')
+        client = self.identity_vault_client.get("client")
 
         result = client.query(
-            TableName=self.identity_vault_client.get('arn').split('/')[1],
-            IndexName='{}-sequence_number'.format(self.identity_vault_client.get('arn').split('/')[1]),
-            Select='ALL_ATTRIBUTES',
-            KeyConditionExpression='sequence_number = :sequence_number',
-            ExpressionAttributeValues={
-                ':sequence_number': {'S': self.sequence_number}
-            }
+            TableName=self.identity_vault_client.get("arn").split("/")[1],
+            IndexName="{}-sequence_number".format(self.identity_vault_client.get("arn").split("/")[1]),
+            Select="ALL_ATTRIBUTES",
+            KeyConditionExpression="sequence_number = :sequence_number",
+            ExpressionAttributeValues={":sequence_number": {"S": self.sequence_number}},
         )
 
         return result
@@ -166,9 +162,7 @@ class Status(object):
     @property
     def all(self):
         """Run all checks and return the results for the given sequence number as a dict."""
-        return {
-            'identity_vault': self.check_identity_vault()
-        }
+        return {"identity_vault": self.check_identity_vault()}
 
     def check_identity_vault(self):
         """Check the sequence number of the last record put to the identity vault."""
@@ -176,7 +170,7 @@ class Status(object):
             self._connect()
 
         query_result = self.query()
-        if len(query_result.get('Items')) == 1:
+        if len(query_result.get("Items")) == 1:
             return True
         else:
             return False
