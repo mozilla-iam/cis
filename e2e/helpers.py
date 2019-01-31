8689848446e0a04b6eca0bb145283b91440d6229
andrewkrug <akrug@mozilla.com>
2019-01-31 14:25:43 -0800

    WIP on fixing profiles generated by faker

--- e2e/helpers.py
+++ e2e/helpers.py
@@ -1,5 +1,9 @@
 import boto3
 import os
+import random
+import cis_crypto
+from cis_profile import common
+from cis_profile import profile
 
 
 def get_cis_environment():
@@ -45,3 +49,69 @@ def get_client_secret():
 
 def get_client_id():
     return get_secure_parameter(get_client_id_path())
+
+def get_complex_structures():
+    return [
+        'staff_information',
+        'access_information',
+        'identities',
+        'schema'
+    ]
+
+def ensure_appropriate_publishers_and_sign(fake_profile, condition):
+    os.environ['CIS_SECRET_MANAGER'] = 'aws-ssm'
+    os.environ['CIS_SECRET_MANAGER_SSM_PATH'] = '/iam/cis/{}'.format(os.getenv('CIS_ENVIRONMENT', 'development'))
+
+    publisher_rules = common.WellKnown().get_publisher_rules()
+    complex_structures = get_complex_structures()
+
+    temp_profile = fake_profile.as_dict()
+    for attr in publisher_rules[condition]:
+        if attr not in complex_structures:
+            successful_random_publisher = random.choice(publisher_rules[condition][attr])
+            temp_profile[attr]['signature']['publisher']['name'] = successful_random_publisher
+            os.environ['CIS_SIGNING_KEY_NAME'] = 'mozilliansorg_signing_key'
+            u = profile.User(user_structure_json=temp_profile)
+            if successful_random_publisher == 'mozilliansorg':
+                os.environ['CIS_SIGNING_KEY_NAME'] == 'mozilliansorg_signing_key'
+            if successful_random_publisher == 'hris':
+                os.environ['CIS_SIGNING_KEY_NAME'] = 'hris_signing_key'
+            if successful_random_publisher == 'ldap':
+                os.environ['CIS_SIGNING_KEY_NAME'] = 'ldap_signing_key'
+            if successful_random_publisher == 'cis':
+                os.environ['CIS_SIGNING_KEY_NAME'] = 'change_service_signing_key'
+            if successful_random_publisher  == 'access_provider':
+                os.environ['CIS_SIGNING_KEY_NAME'] = 'auth0_signing_key'
+            u.sign_attribute(attr, successful_random_publisher)
+            temp_profile = u.as_dict()
+        else:
+            if attr != 'schema' and attr in complex_structures:
+                for k in temp_profile[attr]:
+                    if attr == 'access_information':
+                        successful_random_publisher = random.choice(publisher_rules[condition][attr][k])
+
+                    if attr == 'staff_information' or attr == 'identities':
+                        successful_random_publisher = random.choice(publisher_rules[condition][attr])
+
+                    if successful_random_publisher == 'mozilliansorg':
+                        os.environ['CIS_SIGNING_KEY_NAME'] == 'mozilliansorg_signing_key'
+                    if successful_random_publisher == 'hris':
+                        os.environ['CIS_SIGNING_KEY_NAME'] = 'hris_signing_key'
+                    if successful_random_publisher == 'ldap':
+                        os.environ['CIS_SIGNING_KEY_NAME'] = 'ldap_signing_key'
+                    if successful_random_publisher == 'cis':
+                        os.environ['CIS_SIGNING_KEY_NAME'] = 'change_service_signing_key'
+                    if successful_random_publisher  == 'access_provider':
+                        os.environ['CIS_SIGNING_KEY_NAME'] = 'auth0_signing_key'
+
+                    temp_profile[attr][k]['signature']['publisher']['name'] = successful_random_publisher
+                    u = profile.User(user_structure_json=temp_profile)
+
+                    attribute = '{}.{}'.format(attr, k)
+                    u.sign_attribute(attribute, successful_random_publisher)
+
+                    from pprint import pprint
+                    print(pprint(u.as_dict()))
+                    temp_profile = u.as_dict()
+
+    return profile.User(user_structure_json=temp_profile)
--- e2e/test_change_endpoint.py
+++ e2e/test_change_endpoint.py
@@ -2,10 +2,15 @@ import json
 import jsonschema
 import http.client
 import logging
+import os
+import random
 from . import helpers
+import cis_crypto
+from cis_profile import common
 from cis_profile import fake_profile
+from cis_profile import profile
 from cis_profile import WellKnown
-
+from cis_profile.exceptions import PublisherVerificationFailure
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logger = logging.getLogger(__name__)
@@ -31,6 +36,8 @@ def exchange_for_access_token():
 def test_publishing_a_profile_it_should_be_accepted():
     base_url = helpers.get_url_dict().get('change')
     u = fake_profile.FakeUser()
+    u = helpers.ensure_appropriate_publishers_and_sign(fake_profile=u, condition='create')
+    u.verify_all_publishers(profile.User(user_structure_json=None))
     json_payload = u.as_json()
     wk = WellKnown()
     jsonschema.validate(json.loads(json_payload), wk.get_schema())
@@ -44,15 +51,55 @@ def test_publishing_a_profile_it_should_be_accepted():
     assert data.get('status') == 200
     assert data.get('sequence_number') is not None
 
-
+"""
 def test_publishing_profiles_it_should_be_accepted():
+    os.environ["CIS_SECRET_MANAGER_SSM_PATH"] = "/iam/cis/{}".format(os.getenv('CIS_ENVIRONMENT', 'development'))
     base_url = helpers.get_url_dict().get('change')
     profiles = []
+    publishers = ['ldap', 'cis', 'access_provider', 'mozilliansorg', 'hris']
     while len(profiles) != 5:
         u = fake_profile.FakeUser()
-        if u.verify_all_publishers(u):
-            print(u.verify_all_publishers(u))
+        try:
+            u = helpers.ensure_appropriate_publishers(fake_profile=u, condition='create')
+            u.verify_all_publishers(previous_user=profile.User())
+            os.environ['CIS_SECRET_MANAGER'] = 'aws-ssm'
+            os.environ['CIS_SECRET_MANAGER_SSM_PATH'] = '/iam/cis/{}'.format(os.getenv('CIS_ENVIRONMENT', 'development'))
+            os.environ['CIS_SIGNING_KEY_NAME'] = 'mozilliansorg_signing_key'
+
+            u = u.as_dict()
+            u = profile.User(user_structure_json=u)
+            u.sign_all(publisher_name='mozilliansorg')
+
+            os.environ['CIS_SIGNING_KEY_NAME'] = 'hris_signing_key'
+
+            u = u.as_dict()
+            u = profile.User(user_structure_json=u)
+            u.sign_all(publisher_name='hris')
+
+            os.environ['CIS_SIGNING_KEY_NAME'] = 'ldap_signing_key'
+
+            u = u.as_dict()
+            u = profile.User(user_structure_json=u)
+            u.sign_all(publisher_name='ldap')
+
+            os.environ['CIS_SIGNING_KEY_NAME'] = 'auth0_signing_key'
+
+            u = u.as_dict()
+            print(u['user_id'])
+            u = profile.User(user_structure_json=u)
+            u.sign_all(publisher_name='access_provider')
+
+            os.environ['CIS_SIGNING_KEY_NAME'] = 'change_service_signing_key'
+
+            u = u.as_dict()
+            u = profile.User(user_structure_json=u)
+            u.sign_all(publisher_name='cis')
+            print(u['user_id'])
+
             profiles.append(u.as_json())
+        except PublisherVerificationFailure:
+            pass
+
     wk = WellKnown()
     access_token = exchange_for_access_token()
     conn = http.client.HTTPSConnection(base_url)
@@ -60,6 +107,6 @@ def test_publishing_profiles_it_should_be_accepted():
     conn.request("POST", "/v2/users", json.dumps(profiles), headers=headers)
     res = conn.getresponse()
     data = json.loads(res.read().decode())
-    print(data)
     assert data[0].get('sequence_numbers') is not None
     assert data[0].get('status') == 200
+"""
