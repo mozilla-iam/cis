81f1725a90d275d40e84f083584177f37cd51d06
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-02-04 17:21:43 -0800

    support dev pub keys in allizom.org / dev endpoint

--- well-known-endpoint/tests/test_format.py
+++ well-known-endpoint/tests/test_format.py
@@ -3,5 +3,6 @@ import json
 
 class TestFormat(object):
     def test_json_format(self):
-        json.load(open('tpl/mozilla-iam'))
-        json.load(open('tpl/mozilla-iam-publisher-rules'))
+        json.load(open("tpl/dev.mozilla-iam"))
+        json.load(open("tpl/prod.mozilla-iam"))
+        json.load(open("tpl/mozilla-iam-publisher-rules"))
