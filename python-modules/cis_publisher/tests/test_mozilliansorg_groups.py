606d15ec1e1f2645c87cf271f130ec8b7319c4bb
Florian Merz <flomerz@gmail.com>
2019-08-26 15:46:28 +0200

    skip non-existing profiles

--- python-modules/cis_publisher/tests/test_mozilliansorg_groups.py
+++ python-modules/cis_publisher/tests/test_mozilliansorg_groups.py
@@ -31,25 +31,22 @@ EVENT = {
 
 
 class FakeGetResponse:
-    def __init__(self, ret=User()):
+    def __init__(self, ret=User().as_json(), ok=True):
         self.ret = ret
+        self.ok = ok
+        self.text = ""
 
     def json(self):
-        return json.loads(self.ret.as_json())
-
-    def ok(self):
-        return True
+        return json.loads(self.ret)
 
 
 class FakePostResponse:
+    def __init__(self):
+        self.status_code = 200
+        self.ok = True
+
     def json(self):
         return json.loads("[]")
-
-    def ok(self):
-        return True
-
-    def status_code(self):
-        return 200
 
 
 class TestMozilliansorgGroups:
@@ -72,7 +69,7 @@ class TestMozilliansorgGroups:
         mock_secrets.return_value = "is_pretty_cool"
 
         mock_request_post.return_value = FakePostResponse()
-        mock_request_get.return_value = FakeGetResponse(User(user_id="email|123"))
+        mock_request_get.return_value = FakeGetResponse(User(user_id="email|123").as_json())
 
         update = cis_publisher.MozilliansorgGroupUpdate.from_record(EVENT["Records"][0])
         mozilliansorg_group_publisher = cis_publisher.MozilliansorgGroupsPublisher()
@@ -85,12 +82,30 @@ class TestMozilliansorgGroups:
     @mock.patch("cis_publisher.Publish._request_get")
     @mock.patch("cis_publisher.secret.Manager.secret")
     @mock.patch("cis_publisher.secret.AuthZero.exchange_for_access_token")
+    def test_prepare_update_on_not_existing_profile(
+        self, mock_authzero, mock_secrets, mock_request_get, mock_request_post
+    ):
+        mock_authzero.return_value = "dinopark"
+        mock_secrets.return_value = "is_pretty_cool"
+
+        mock_request_post.return_value = FakePostResponse()
+        mock_request_get.return_value = FakeGetResponse(ret="{}", ok=False)
+
+        update = cis_publisher.MozilliansorgGroupUpdate.from_record(EVENT["Records"][0])
+        mozilliansorg_group_publisher = cis_publisher.MozilliansorgGroupsPublisher()
+        update_profile = mozilliansorg_group_publisher._prepare_update(update)
+        assert update_profile is None
+
+    @mock.patch("cis_publisher.Publish._request_post")
+    @mock.patch("cis_publisher.Publish._request_get")
+    @mock.patch("cis_publisher.secret.Manager.secret")
+    @mock.patch("cis_publisher.secret.AuthZero.exchange_for_access_token")
     def test_publish(self, mock_authzero, mock_secrets, mock_request_get, mock_request_post):
         mock_authzero.return_value = "dinopark"
         mock_secrets.return_value = "is_pretty_cool"
 
         mock_request_post.return_value = FakePostResponse()
-        mock_request_get.return_value = FakeGetResponse(User(user_id="email|123"))
+        mock_request_get.return_value = FakeGetResponse(User(user_id="email|123").as_json())
 
         mozilliansorg_group_publisher = cis_publisher.MozilliansorgGroupsPublisher()
         mozilliansorg_group_publisher.publish(EVENT)
