71cc924f4c4c35c84f974b47497f659ac38ba042
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-09-13 12:48:59 -0700

    bump package to 0.4.0 for upload

--- python-modules/cis_profile/setup.py
+++ python-modules/cis_profile/setup.py
@@ -23,7 +23,7 @@ extras = {"test": test_requirements}
 
 setup(
     name="cis_profile",
-    version="0.3.9",
+    version="0.4.0",
     author="Guillaume Destuynder",
     author_email="kang@mozilla.com",
     description="Mozilla IAM user profile ('v2') class utility.",
