1d8c3e37974e263a00e0a643331b9b01e6a2538d
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-10-21 17:01:45 -0700

    fix notifier test

--- python-modules/cis_notifications/tests/test_secret.py
+++ python-modules/cis_notifications/tests/test_secret.py
@@ -1,5 +1,6 @@
 import boto3
 from moto import mock_ssm
+from moto import mock_secretsmanager
 from os import environ
 
 
@@ -9,6 +10,7 @@ class TestAccessTokenExchange(object):
 
 
 @mock_ssm
+@mock_secretsmanager
 class TestSecret(object):
     def test_secret_manager(self):
         self.secret_name = "dinosecret"
@@ -27,3 +29,16 @@ class TestSecret(object):
         manager = secret.Manager()
         result = manager.secret("dinosecret")
         assert result == "adinosecret"
+
+    # XXX Not yet implemented in moto
+    # def test_secretmgr_manager(self):
+    # self.secret_name = "dinosecret"
+    # client = boto3.client("secretsmanager", region_name="us-west-2")
+    # client.update_secret(SecretId="/baz/{}".format(self.secret_name), SecretString="adinosecret")
+
+    # from cis_notifications import secret
+
+    # environ["CIS_SECRET_MANAGER_SSM_PATH"] = "/baz"
+    # manager = secret.Manager()
+    # result = manager.secretmsg("dinosecret")
+    # assert result == "adinosecret"
