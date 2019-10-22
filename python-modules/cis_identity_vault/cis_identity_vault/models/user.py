44cf3bad7df57ae3774b70e2b8ca6aa986308911
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-10-22 13:37:50 -0700

    boost max threads as we currently need 30 segments for the testing table 50 seems to be a decent balance of performance vs hammering dynamodb

--- python-modules/cis_identity_vault/cis_identity_vault/models/user.py
+++ python-modules/cis_identity_vault/cis_identity_vault/models/user.py
@@ -363,7 +363,7 @@ class Profile(object):
         """
         results = queue.Queue()
         threads = []
-        max_threads = 10
+        max_threads = 50
         users = []
         segment_result = None
         # We're choosing to divide the table in 3, then...
