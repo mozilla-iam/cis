e319b9f93dd976d6b3e64960ccd2594984eafd00
Bheesham Persaud <bhee@mozilla.com>
2025-07-08 14:39:43 -0400

    Move build resources to a separate state location
    Jira: IAM-1690

--- terraform/build/provider.tf
+++ terraform/build/provider.tf
@@ -1,13 +1,3 @@
-# DEBT(bhee): There's now a _bunch_ of different places we have Terraform.
-# There's been _some_ effort to centralize where, but I'm making an explicit
-# call to keep CIS-related things in the CIS repository.
-#
-# This almost follows the pattern from dino park, except we also have
-# mozilla-iam/iam-infra.
-#
-# There is no right decision here, only less wrong; and this is the one I made.
-
-
 # Locked with:
 # terraform providers lock -platform darwin_amd64 -platform darwin_arm64 -platform linux_amd64 -platform linux_arm64
 terraform {
@@ -16,7 +6,7 @@ terraform {
     # Re-using the one from mozilla-iam/iam-infra, to save having multiple
     # places to audit.
     bucket = "eks-terraform-shared-state"
-    key    = "cis/terraform/terraform.tfstate"
+    key    = "cis/terraform/build/terraform.tfstate"
     region = "us-west-2"
   }
   required_providers {
