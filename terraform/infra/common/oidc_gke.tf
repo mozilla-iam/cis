faa5bdf9ddbedbbcc3ac2845912d6d1d7b4dac2a
Bheesham Persaud <bhee@mozilla.com>
2025-08-07 23:18:25 -0400

    feat(terraform): Our GKE workloads will require AWS access
    Jira: IAM-1735

---
+++ terraform/infra/common/oidc_gke.tf
@@ -0,0 +1,17 @@
+# From:
+# https://github.com/mozilla/global-platform-admin/blob/d3ba91536f92d2de15c9fc0f22a216f0d6cadc59/platform-shared/k8s/bootstrap/webservices-high-public-nonprod-us-west1.yaml
+module "oidc_gke_webservices_high_public_nonprod" {
+  source           = "github.com/mozilla/terraform-modules//aws_gke_oidc_config?ref=aws_gke_oidc_config-0.1.0"
+  gcp_region       = "us-west1"
+  gcp_project_id   = "moz-fx-webservices-high-nonpro"
+  gke_cluster_name = "webservices-high-public-nonprod-us-west1"
+}
+
+# From:
+# https://github.com/mozilla/global-platform-admin/blob/d3ba91536f92d2de15c9fc0f22a216f0d6cadc59/platform-shared/k8s/bootstrap/webservices-high-public-prod-us-west1.yaml
+module "oidc_gke_webservices_high_public_prod" {
+  source           = "github.com/mozilla/terraform-modules//aws_gke_oidc_config?ref=aws_gke_oidc_config-0.1.0"
+  gcp_region       = "us-west1"
+  gcp_project_id   = "moz-fx-webservices-high-prod"
+  gke_cluster_name = "webservices-high-public-prod-us-west1"
+}
