cceb71393e2b09a7dae221cfafcf83a3b4ab5a50
Bheesham Persaud <bhee@mozilla.com>
2025-08-08 00:30:53 -0400

    feat(terraform): Those GKE workloads need a role
    Jira: IAM-1735

---
+++ terraform/infra/dev/iam_gke.tf
@@ -0,0 +1,37 @@
+# Depends on ../common.
+
+data "aws_region" "current" {}
+
+# From: serverless-functions/profile_retrieval/serverless.yml
+data "aws_iam_policy_document" "dynamo_read" {
+  statement {
+    sid = "AllowRead"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+    ]
+    resources = [
+      aws_dynamodb_table.dynamo.arn,
+      "${aws_dynamodb_table.dynamo.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "cis_dynamo_read" {
+  name   = "CISDynamoRead"
+  policy = data.aws_iam_policy_document.dynamo_read.json
+}
+
+module "cis_profile_retrieval_api" {
+  source              = "github.com/mozilla/terraform-modules//aws_gke_oidc_role?ref=aws_gke_oidc_config-0.1.0"
+  role_name           = "GKE${title(var.environment)}CISProfileRetrievalAPI"
+  aws_region          = data.aws_region.current.region
+  gcp_region          = var.gcp_region
+  gke_cluster_name    = var.gke_cluster_name
+  gcp_project_id      = var.gcp_project_id
+  gke_namespace       = var.gke_namespace
+  gke_service_account = "cis-profile-retrieval-api"
+  iam_policy_arns     = [aws_iam_policy.cis_dynamo_read.arn]
+}
--- terraform/infra/dev/variables.tf
+++ terraform/infra/dev/variables.tf
@@ -1,3 +1,19 @@
 variable "environment" {
   type = string
 }
+
+variable "gcp_region" {
+  type = string
+}
+
+variable "gke_cluster_name" {
+  type = string
+}
+
+variable "gcp_project_id" {
+  type = string
+}
+
+variable "gke_namespace" {
+  type = string
+}
